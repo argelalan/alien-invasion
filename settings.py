e7ab3319cf7e99a87871550bef792769d9e81784
argelalan <campos.alan@protonmail.com>
2021-06-22 15:23:42 -0700

    Added ship and game controls.

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ class Settings:
     """Class that stores all settings for Alien Invasion game."""
 
     def __init__(self):
-        self.height = 1200
-        self.width = 800
+        self.screen_height = 1200
+        self.screen_width = 800
         self.bg_color = (0, 2, 42)
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -8,11 +8,29 @@ class Ship:
         """Initialize the ship and get its starting position"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         self.image = pygame.image.load('images/ship_75.bmp')
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store decimal value for ship's horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flags.
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
+
     def blitme(self):
+        """Draw ship to screen."""
         self.screen.blit(self.image, self.rect)
